482dc11d9d013f12cff852c23fc57b0fb298e124
Nicolas Gouwy <43481095+Iconejey@users.noreply.github.com>
2024-10-13 20:44:21 +0000

    Include Nosuite auth

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,17 @@
-console.log('Hello from express template!');
+$('#auth').onclick = async () => {
+	const success = await authenticate(true);
+	if (success) onAuth();
+};
+
+async function onAuth() {
+	$('#out').disabled = false;
+
+	const data = await getAccountInfo();
+	const { name, email } = data;
+	$('#name').innerText = name;
+	$('#email').innerText = email;
+}
+
+$('#out').onclick = signOut;
+
+if (userSignedIn()) onAuth();
